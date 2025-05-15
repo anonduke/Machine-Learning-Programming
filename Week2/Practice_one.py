529dd7218b877381b90a4c9fe9841b16dbbc81e3
anonduke <fasalufrkp@gmail.com>
2025-05-15 12:57:48 -0400

    InCLass Activity

---
+++ Week2/Practice_one.py
@@ -0,0 +1,19 @@
+#--------------------------------------------
+print("-----Challenge 1------")
+def findreminder(num1, num2):
+    result = num1 % num2
+    return result
+print(findreminder(10, 2))
+print(findreminder(10, 3))
+#-----------------------------------------------
+print("-----Challenge 2------")
+def greeting(name):
+    print("Hello there" + name )
+greeting(" John")
+#-----------------------------------------------
+print("-----Challenge 3------")
+def areafind(radius):
+    area = 3.14 * radius * radius
+    return area
+print(areafind(5))
+#-----------------------------------------------
